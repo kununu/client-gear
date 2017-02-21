da3c185660dbadb8ad15083d3ebfb89b1ada5572
Karl Horky <karl.horky@gmail.com>
2017-02-21 14:01:47 +0100

    Add lint script, fix linting errors

--- packages/kununu-logo/Logo.test.jsx
+++ packages/kununu-logo/Logo.test.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer'; // eslint-disable-line import/no-extraneous-dependencies
 
 import KununuLogo from '.';
 
 test('Renders Logo without crashing', () => {
-  const component = renderer.create(
-    <KununuLogo />
-  );
-
+  const component = renderer.create(<KununuLogo />);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
-
