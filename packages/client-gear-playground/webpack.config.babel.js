88c9d099015f5a8e52cddb889f3acb5f3f0ba0f4
Catarina Lopes <clopes@ventureoak.com>
2017-06-29 17:27:38 +0100

    Add space after footer links

--- packages/client-gear-playground/webpack.config.babel.js
+++ packages/client-gear-playground/webpack.config.babel.js
@@ -80,7 +80,7 @@ module.exports = {
         loader: 'url?limit=10000&mimetype=application/font-woff',
       },
       {
-        test: /\.(ttf|eot|svg|gif)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        test: /\.(ttf|eot|svg|gif|png)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         loader: 'file',
       },
     ],
