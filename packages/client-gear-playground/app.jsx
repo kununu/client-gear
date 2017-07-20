1ca8b530f51aef4764033bbfba8c1039bfbd5a26
Catarina Lopes <clopes@ventureoak.com>
2017-07-20 13:50:42 +0100

    Update dependencies

--- packages/client-gear-playground/app.jsx
+++ packages/client-gear-playground/app.jsx
@@ -1,8 +1,8 @@
 import React, {PropTypes} from 'react';
 import {Link} from 'react-router';
 
-import {Header, HeaderNav, HeaderNavItem} from '../kununu-header';
-import Footer from '../kununu-footer';
+import {Header, HeaderNav, HeaderNavItem} from '../kununu-header/index';
+import Footer from '../kununu-footer/index';
 
 // if you want to test the distribution, just uncomment the lines below
 /* import Header from '../kununu-header/dist/Header';
