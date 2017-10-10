da15770e254b593fd0530393b92a418120c4b9ef
Kaitlin Rathwell <kaytcat@gmail.com>
2017-10-10 13:32:23 +0200

    Reorganize stuff

--- packages/kununu-utils/src/emailValidation/index.js
+++
@@ -1,4 +0,0 @@
-export default function translate (value) {
-  const emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return emailPattern.test(value);
-}
