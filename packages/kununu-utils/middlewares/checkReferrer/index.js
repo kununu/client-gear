f52007f887d7842b907f7c29b4f158509aeb5973
Daniel Stein <stein-daniel@codingcircus.com>
2018-02-07 15:30:09 +0100

    Removed console log

--- packages/kununu-utils/middlewares/checkReferrer/index.js
+++ packages/kununu-utils/middlewares/checkReferrer/index.js
@@ -3,7 +3,6 @@ import getDomain from '../../express-helpers/getDomain';
 const referrerCookie = 'referrer';
 
 export default () => (req, res, next) => {
-  console.log(req.query);
   if (req.query.rfr) {
     res.cookie(referrerCookie, req.query.rfr, {
       domain: getDomain(req),
