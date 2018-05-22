96aeeaf0a2c92b0c42d901c0315fc15a1faa9e6b
Alex Khalevine <alex.khalevine@kununu.com>
2018-05-22 16:39:10 +0200

    moved formValidation utils from app-profiles to client gear

--- packages/kununu-form-wrapper/index.jsx
+++ packages/kununu-form-wrapper/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-import {validateField} from '../kununu-utils/kununu-helpers/formValidation';
+import {validateField} from '@kununu/kununu-utils/dist/kununu-helpers/formValidation';
 
 const FormWrapper = (WrappedComponent) => {
   class HOC extends React.Component {
