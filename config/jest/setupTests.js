2aa5d27abfc0985f91970f0e66ac2d86a9cd3929
Alex Khalevine <alex.khalevine@kununu.com>
2018-05-23 15:40:44 +0200

    added react-dom to top level package to resolve ReactSixteenAdapter errors

--- config/jest/setupTests.js
+++ config/jest/setupTests.js
@@ -1,10 +1,8 @@
-import raf from './shim';
+// Temporary hack to suppress error
+// https://github.com/facebook/jest/issues/4545
+import shim from './shim';
 
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({adapter: new Adapter()});
-
-global.requestAnimationFrame = (callback) => {
-  setTimeout(callback, 0);
-};
--- config/jest/shim.js
+++ config/jest/shim.js
@@ -1,9 +1,8 @@
 // Temporary hack to suppress error
 // https://github.com/facebook/jest/issues/4545
 
-
-const raf = global.requestAnimationFrame = (cb) => {
+const shim = global.requestAnimationFrame = (cb) => {
   setTimeout(cb, 0);
 };
 
-export default raf;
+export default shim;
