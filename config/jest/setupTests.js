0cc744fc5cda5c451c63a829b96066138fc647d6
Alex Khalevine <alex.khalevine@kununu.com>
2018-05-24 10:21:23 +0200

    set global react back to 15 in order to get all packages to compile

--- config/jest/setupTests.js
+++ config/jest/setupTests.js
@@ -3,6 +3,6 @@
 import shim from './shim';
 
 import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from 'enzyme-adapter-react-15';
 
 Enzyme.configure({adapter: new Adapter()});
