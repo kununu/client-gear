8656ad4cb5adb8f3664ec2a417e5bec6a73fefa3
Pedro Menezes <pedro.menezes@kununu.com>
2018-06-08 10:32:24 -0300

    Add new icons

---
+++ packages/kununu-icons/FlagCh.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Icon = ({className}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 22 16"
+    className={className}
+  >
+    <rect
+      style={{fill: '#FF0000'}}
+      x="0"
+      y="0"
+      width="22"
+      height="16"
+    />
+    <polygon
+      style={{fill: '#FFFFFF'}}
+      points="6,6 9.1,6 9.1,3 12.9,3 12.9,6 16,6 16,9.8 12.9,9.8 12.9,13 9.1,13 9.1,9.8 6,9.8"
+    />
+  </svg>
+);
+
+Icon.propTypes = {
+  className: PropTypes.string,
+};
+
+Icon.defaultProps = {
+  className: '',
+};
+
+export default Icon;
---
+++ packages/kununu-icons/FlagDe.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Icon = ({className}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 22 16"
+    className={className}
+  >
+    <path
+      style={{fill: '#FE0000'}}
+      d="M0,5.3c7.3,0,14.7,0,22,0c0,1.8,0,3.6,0,5.3c-7.3,0-14.7,0-22,0C0,8.9,0,7.1,0,5.3z"
+    />
+    <path
+      style={{fill: '#FECB00'}}
+      d="M0,10.7c7.3,0,14.7,0,22,0c0,1.8,0,3.6,0,5.3c-7.3,0-14.7,0-22,0C0,14.2,0,12.4,0,10.7z"
+    />
+    <path d="M0,0c7.3,0,14.7,0,22,0c0,1.8,0,3.6,0,5.3c-7.3,0-14.7,0-22,0C0,3.6,0,1.8,0,0z" />
+  </svg>
+);
+
+Icon.propTypes = {
+  className: PropTypes.string,
+};
+
+Icon.defaultProps = {
+  className: '',
+};
+
+export default Icon;
