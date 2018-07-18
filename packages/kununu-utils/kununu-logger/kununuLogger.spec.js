3dd677f7a79628d1fed4e332caef6b1f7318c609
Kaitlin Rathwell <kaytcat@gmail.com>
2018-07-18 17:02:59 +0200

    KUNTECH-3277 Add logger (#69)
    * Add logger using winston
    
    * Fix and add tests
    
    * Update node version to use
    
    * Adjust test so I don't have to test the date
    
    * Add kibana format and new test

---
+++ packages/kununu-utils/kununu-logger/kununuLogger.spec.js
@@ -0,0 +1,89 @@
+import {formatNodeRequest, logger, customFormat} from './index';
+
+const express = require('express');
+const request = require('supertest');
+
+describe('Returns correct log format with text format', () => {
+  const app = express();
+
+  it('returns correct format for express request logs', async () => {
+    const label = 'test1';
+
+    app.get('/', (req, res) => {
+      const expectedRequest = {
+        label,
+        time: new Date().toISOString(),
+        method: req.method,
+        request: req.originalUrl,
+        status: res.statusCode,
+        remote_ip: '::ffff:127.0.0.1',
+        referer: '-',
+        forwarded_for: '-',
+        trace_id: '-',
+        logType: 'middleware_logger',
+      };
+
+      res.send({
+        formatedRequest: formatNodeRequest({req, res, label}),
+        expectedRequest,
+      });
+    });
+
+    const response = await request(app).get('/');
+    expect(response.formatedRequest).toEqual(JSON.stringify(response.expectedRequest));
+  });
+
+  it('returns expected format for custom request logs', async () => {
+    const label = 'test2';
+    let generatedLog = '';
+
+    const spyFunc = jest.fn((val) => {
+      generatedLog = JSON.parse(val.slice(val.indexOf('{')));
+    });
+
+    global.console = {log: spyFunc};
+
+    app.get('/2', (req, res) => {
+      logger.log('info', {
+        custom: true,
+        message: 'Test',
+        label,
+      });
+
+      res.send();
+    });
+
+    await request(app).get('/2');
+    expect(generatedLog.custom).toEqual(true);
+    expect(generatedLog.message).toEqual('Test');
+    expect(generatedLog.level).toEqual('info');
+    expect(generatedLog.label).toEqual(label);
+  });
+});
+
+describe('Returns correct log format with json format', () => {
+  beforeEach(() => {
+    process.env.LOG_AS = 'json';
+  });
+
+  afterEach(() => {
+    delete process.env.LOG_AS;
+  });
+
+  it('returns correct log', () => {
+    const info = {
+      label: 'test',
+      level: 'info',
+      custom: true,
+    };
+
+    const printf = customFormat;
+    const value = printf.template(info);
+    expect(JSON.parse(value)).toEqual({
+      label: 'test',
+      level: 'info',
+      custom: true,
+      logType: 'custom_logger',
+    });
+  });
+});
