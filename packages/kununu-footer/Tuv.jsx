c40c24dd57b10124a53d434a73c65aa3ff9b7861
Daniel Stein <stein-daniel@codingcircus.com>
2018-08-21 13:36:06 +0200

    Use cdn for tuv logo (#79)
    * uses the assets cdn for the tuv logo
    
    * Use prop for assetsPath

---
+++ packages/kununu-footer/Tuv.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Tuv = ({className, assetsPath}) => (
+  <div className={className}>
+    <img src={`${assetsPath}/tuv.svg`} alt="TUV Logo" />
+  </div>
+);
+
+Tuv.propTypes = {
+  className: PropTypes.string,
+  assetsPath: PropTypes.string.isRequired,
+};
+
+Tuv.defaultProps = {className: ''};
+
+export default Tuv;
