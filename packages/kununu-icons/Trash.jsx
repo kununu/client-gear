75e1179f08c9c1618252b938faf19ea4bdc67511
Pedro Menezes <pedro.menezes@kununu.com>
2018-12-28 14:17:02 +0000

    Add Trash icon; Bump version

---
+++ packages/kununu-icons/Trash.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Icon = ({className, ariaHidden}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 512 512"
+    className={className}
+    aria-hidden={ariaHidden}
+  >
+    <path d="M4 10v20c0 1.1 0.9 2 2 2h18c1.1 0 2-0.9 2-2v-20h-22zM10 28h-2v-14h2v14zM14 28h-2v-14h2v14zM18 28h-2v-14h2v14zM22 28h-2v-14h2v14z"></path>
+    <path d="M26.5 4h-6.5v-2.5c0-0.825-0.675-1.5-1.5-1.5h-7c-0.825 0-1.5 0.675-1.5 1.5v2.5h-6.5c-0.825 0-1.5 0.675-1.5 1.5v2.5h26v-2.5c0-0.825-0.675-1.5-1.5-1.5zM18 4h-6v-1.975h6v1.975z"></path>
+  </svg>
+);
+
+Icon.propTypes = {
+  className: PropTypes.string,
+  ariaHidden: PropTypes.bool,
+};
+
+Icon.defaultProps = {
+  className: '',
+};
+
+export default Icon;
