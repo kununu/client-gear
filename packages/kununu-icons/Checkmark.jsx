adf623ea3dc30d7be4c85ac97718ff915c68081f
Pedro Menezes <36070274+pedrommenezes@users.noreply.github.com>
2019-01-04 02:38:56 +0000

    [KUNFXTCH-261] Add new icons (#95)
    * Add new Minus icon
    
    * Bump version; Update dependencies
    
    * Update Minus icon; Add CheckSuccess and rename old one to CheckSuccessOutline
    
    * Add Trash icon; Bump version
    
    * Fix CheckSuccess icon; Bump version
    
    * Fix Minus icon
    
    * Bump version
    
    * Fix Trash icon
    
    * Add ArrowLeft and ArrowRight icons; Bump version
    
    * Bump version
    
    * Fix ESLint errors
    
    * Rename CheckSuccess to Checkmark and CheckSuccessOutline to CheckSuccess
    
    * Add Checkmark icon
    
    * Update Babel dependencies
    
    * Add @babel/preset-react

---
+++ packages/kununu-icons/Checkmark.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Icon = ({className, ariaHidden}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 32 32"
+    className={className}
+    aria-hidden={ariaHidden}
+  >
+    <path d="M27 4l-15 15-7-7-5 5 12 12 20-20z" />
+  </svg>
+);
+
+Icon.propTypes = {
+  className: PropTypes.string,
+  ariaHidden: PropTypes.bool,
+};
+
+Icon.defaultProps = {
+  className: '',
+};
+
+export default Icon;
