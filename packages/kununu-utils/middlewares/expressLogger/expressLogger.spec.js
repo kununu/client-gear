436f5bc7bbc31a497d742b42bf4294d366a11b71
Pedro Menezes <pedro.menezes@kununu.com>
2019-02-01 18:10:28 +0000

    Remove sort to turn expect less verbose

--- packages/kununu-utils/middlewares/expressLogger/expressLogger.spec.js
+++ packages/kununu-utils/middlewares/expressLogger/expressLogger.spec.js
@@ -31,7 +31,7 @@ describe('Express logger', () => {
     expect(spyFunc.mock.calls.length).toEqual(2);
 
     const logObjectRequest = JSON.parse(spyFunc.mock.calls[0][0]);
-    expect(Object.keys(logObjectRequest).sort()).toMatchObject(['custom', 'label', 'level', 'logType', 'method', 'request', 'timestamp']);
+    expect(Object.keys(logObjectRequest)).toMatchObject(['custom', 'method', 'request', 'label', 'level', 'timestamp', 'logType']);
   });
 
   it('logs a response with the expected info', async () => {
@@ -45,7 +45,7 @@ describe('Express logger', () => {
     expect(spyFunc.mock.calls.length).toEqual(2);
 
     const logObjectResponse = JSON.parse(spyFunc.mock.calls[1][0]);
-    expect(Object.keys(logObjectResponse).sort()).toMatchObject(['build', 'forwarded_for', 'label', 'logType', 'method', 'referer', 'remote_ip', 'request', 'status', 'time', 'time_taken_micros', 'trace_id', 'user_agent']);
+    expect(Object.keys(logObjectResponse)).toMatchObject(['label', 'time', 'method', 'request', 'status', 'remote_ip', 'referer', 'forwarded_for', 'trace_id', 'logType', 'user_agent', 'time_taken_micros', 'build']);
     expect(logObjectResponse.status).toBe(500);
   });
 });
