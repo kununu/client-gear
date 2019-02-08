93ecb47bcbfd9e456a1a74a917dec6fbaa079a3e
Pedro Menezes <pedro.menezes@kununu.com>
2019-02-08 10:31:39 +0000

    Fingers Crossed files

---
+++ packages/kununu-utils/kununu-logger/fingersCrossed.js
@@ -0,0 +1,77 @@
+import {formatNodeRequest} from './index';
+
+const TransportStream = require('winston-transport'); // eslint-disable-line import/no-extraneous-dependencies
+const {LEVEL} = require('triple-beam'); // eslint-disable-line import/no-extraneous-dependencies
+
+/**
+ * Receives Winston logging level and
+ * return its correspondent specified by RFC5424
+ *
+ * @param  {String} winstonLevel
+ * @return {Integer}
+ */
+const getLoggingLevel = (level) => {
+  const levels = {
+    emerg: 0,
+    alert: 1,
+    crit: 2,
+    error: 3,
+    warning: 4,
+    notice: 5,
+    info: 6,
+    debug: 7,
+  };
+
+  return levels[level.toLowerCase()];
+};
+
+module.exports = class FingersCrossed extends TransportStream {
+  constructor (options = {}) {
+    super(options);
+
+    this.name = 'kununu-winston';
+    this.triggerLevel = options.triggerLevel || 'error';
+
+    this.state = [];
+  }
+
+  pushState (log) {
+    this.state.push(log);
+  }
+
+  removeState (traceId) {
+    this.state = this.state.filter(log => traceId !== log.trace_id);
+  }
+
+  log (info, callback) {
+    setImmediate(() => this.emit('logged', info));
+
+    const formatedLog = JSON.parse(formatNodeRequest(info));
+
+    // Store all response logs on state
+    this.pushState(formatedLog);
+
+    // If has required minimum log level, then recover previous logs
+    if (this.hasRequiredMinimumLevel(info)) {
+      const recoverLogs = this.recoverLogs(formatedLog);
+
+      // Remove recovered logs from state
+      this.removeState(formatedLog.trace_id);
+
+      // Output recovered logs with same trace id
+      console.log(recoverLogs); // eslint-disable-line no-console
+    }
+
+    callback();
+  }
+
+  // Check whether request has required minimum log level
+  hasRequiredMinimumLevel (info) {
+    return getLoggingLevel(info[LEVEL]) <= getLoggingLevel(this.triggerLevel);
+  }
+
+  // Recover logs with same trace id as the error
+  recoverLogs (info) {
+    return this.state.filter(log => info.trace_id === log.trace_id);
+  }
+};
---
+++ packages/kununu-utils/kununu-logger/fingersCrossed.spec.js
@@ -0,0 +1,55 @@
+import expressLogger from '../middlewares/expressLogger';
+
+const express = require('express');
+const request = require('supertest');
+
+describe('Fingers Crossed transport for kununu-logger', () => {
+  const app = express();
+
+  app.use(expressLogger('app'));
+
+  app.get('/', (req, res) => {
+    res.send();
+  });
+
+  app.post('/post', (req, res) => {
+    res.send();
+  });
+
+  app.get('/error', (req, res) => {
+    res.status(500).send();
+  });
+
+  it('logs requests and return errors and previous requests that share same trace id', async () => {
+    const spyFunc = jest.fn();
+
+    global.console = {
+      log: spyFunc,
+    };
+
+    await request(app).post('/post').set('x-amzn-trace-id', 'trace-id-1');
+    await request(app).get('/').set('x-amzn-trace-id', 'trace-id-2');
+    await request(app).get('/').set('x-amzn-trace-id', 'trace-id-3');
+    await request(app).get('/error').set('x-amzn-trace-id', 'trace-id-3');
+
+    expect(spyFunc.mock.calls.length).toBe(1); // One console.log call
+    expect(spyFunc.mock.calls[0][0].length).toBe(2); // Two requests logged
+    // Requests share same trace_id
+    expect(spyFunc.mock.calls[0][0][0].trace_id).toBe('trace-id-3');
+    expect(spyFunc.mock.calls[0][0][1].trace_id).toBe('trace-id-3');
+  });
+
+  it('logs requests and return nothing because there are no errors', async () => {
+    const spyFunc = jest.fn();
+
+    global.console = {
+      log: spyFunc,
+    };
+
+    await request(app).post('/post').set('x-amzn-trace-id', 'trace-id-1');
+    await request(app).get('/').set('x-amzn-trace-id', 'trace-id-2');
+    await request(app).get('/').set('x-amzn-trace-id', 'trace-id-3');
+
+    expect(spyFunc.mock.calls.length).toBe(0);
+  });
+});
