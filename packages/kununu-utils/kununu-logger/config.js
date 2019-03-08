7700a6a1a933dda23dae2802c12e32fc9c40ce88
Pedro Menezes <pedro.menezes@kununu.com>
2019-03-08 18:25:17 +0000

    Fix comments about log levels

--- packages/kununu-utils/kununu-logger/config.js
+++ packages/kununu-utils/kununu-logger/config.js
@@ -1,18 +1,23 @@
 /**
- * Minimum level is in use by both Console and Fingers Crossed transports
- * Logs below this level are always discarded
+ * Minimum level is used by Console and Fingers Crossed transports
+ * When minimum log level is set, any log that has
+ * inferior level will be discarded and not processed by transport
+ *
  */
 export const minimumLogLevel = process.env.MINIMUM_LOG_LEVEL || 'info';
 
 /**
  * Activation level is used by Fingers Crossed transport
- * Logs below are only output when this is reached
+ * When activation log level is set, any log that has
+ * equal or superior level will be outputed
+ *
  */
 export const activationLogLevel = process.env.ACTIVATION_LOG_LEVEL || 'error';
 
 /**
  * Custom logging levels
  * This defines new logger method calls, e.g.: logger.emergency() and logger.critical()
+ *
  */
 export const loggingLevels = {
   emergency: 0,
