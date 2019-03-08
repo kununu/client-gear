3f3b48eaef1c9faaea3db8b4e91c946c44774f53
Catarina Lopes <catarina.lopes@kununu.com>
2019-03-08 09:59:21 +0000

    Update jest

--- packages/client-gear-playground/home.jsx
+++ packages/client-gear-playground/home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 // if you want to test the distribution, just uncomment the lines below
-import Overlay from '@kununu/kununu-overlay';
-import Logo from '@kununu/kununu-logo';
+// import Overlay from '@kununu/kununu-overlay';
+// import Logo from '@kununu/kununu-logo';
 
-// import Overlay from '../kununu-overlay/index';
-// import Logo from '../kununu-logo/index';
+import Overlay from '../kununu-overlay/index';
+import Logo from '../kununu-logo/index';
 
 const Home = () => (
   <div style={{position: 'relative'}}>
