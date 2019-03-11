072e0421bc4eb5f205744203fac4352135da5bb1
Pedro Menezes <pedro.menezes@kununu.com>
2019-03-11 12:52:07 +0000

    Implement node-cache to expire state after 10 min

--- packages/kununu-utils/kununu-logger/fingers-crossed/index.js
+++ packages/kununu-utils/kununu-logger/fingers-crossed/index.js
@@ -1,6 +1,7 @@
 import formatNodeRequest from '../format-node-request';
 
 const TransportStream = require('winston-transport');
+const NodeCache = require('node-cache');
 
 module.exports = class FingersCrossed extends TransportStream {
   constructor (options = {}) {
@@ -11,7 +12,8 @@ module.exports = class FingersCrossed extends TransportStream {
     this.activationLogLevel = options.activationLogLevel || 'error';
     this.levels = options.levels;
 
-    this.state = [];
+    // Logs expiration is set to 10 minutes
+    this.cache = new NodeCache({stdTTL: 600, checkperiod: 600});
   }
 
   log (info, callback) {
@@ -24,21 +26,24 @@ module.exports = class FingersCrossed extends TransportStream {
       const traceId = info.req.headers['x-amzn-trace-id'];
 
       if (traceId) {
-        this.statePush(traceId, log);
+        this.saveOnState(traceId, log);
 
+        // Outputs when it has a request, trace ID and reaches activation log level
         if (this.hasActivationLogLevel(logLevel)) {
           const logs = this.recoverLogs(traceId);
 
-          this.cleanState(traceId);
+          this.removeFromState(traceId);
           this.outputLog(logs);
         }
       }
 
+      // Outputs when it has a request, but not a trace ID and has minimum log level
       if (!traceId && this.hasMinimumLogLevel(logLevel)) {
         this.outputLog(log);
       }
     }
 
+    // Outputs when it's not a request but has minimum log level
     if (!info.req && this.hasMinimumLogLevel(logLevel)) {
       this.outputLog(log);
     }
@@ -72,21 +77,29 @@ module.exports = class FingersCrossed extends TransportStream {
   hasMinimumLogLevel = level => this.getLogLevel(level) <= this.getLogLevel(this.level);
 
   /**
-   * Push log to state
+   * Save log on state
    *
    * @param {String} traceId
-   * @param {Object} log
+   * @param {Object} raw
    */
-  statePush = (traceId, log) => this.state.push({trace_id: traceId, formatted_log: log});
+  saveOnState = (traceId, raw) => {
+    let log = [];
+
+    try {
+      log = this.cache.get(traceId, true);
+    } catch (err) {
+      this.cache.set(traceId, []);
+    }
+
+    this.cache.set(traceId, [...log, raw]);
+  }
 
   /**
    * Remove logs with a certain trace ID from state
    *
    * @param {String} traceId
    */
-  cleanState = (traceId) => {
-    this.state = this.state.filter(log => traceId !== log.trace_id);
-  }
+  removeFromState = traceId => this.cache.del(traceId);
 
   /**
    * Recover logs with same trace ID as the error
@@ -94,7 +107,7 @@ module.exports = class FingersCrossed extends TransportStream {
    * @param  {String} traceId
    * @return {Array}
    */
-  recoverLogs = traceId => this.state.filter(log => traceId === log.trace_id);
+  recoverLogs = traceId => this.cache.get(traceId, (err, logs) => !err && Array.isArray(logs) ? logs : []);
 
   /**
    * Output an array of logs individually or just one if it's an Object
