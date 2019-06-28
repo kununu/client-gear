0d90acec431ea2cb75ab1cf198995b91f7955bc9
Kaitlin Rathwell <kaitlin.rathwell@kununu.com>
2019-06-28 16:47:34 +0200

    Add simple version to header

--- packages/kununu-header/Header.jsx
+++ packages/kununu-header/Header.jsx
@@ -11,25 +11,53 @@ export default function Header ({
   assetsPath,
   title,
   logoLink,
+  simple,
 }) {
+  const simpleLogo = () => {
+    const content = (
+      <picture>
+        <source
+          srcSet={`${assetsPath}/logo-desktop.svg`}
+        />
+        <img
+          alt={title}
+          aria-hidden="true"
+          className={styles.simpleLogo}
+          rel="presentation"
+          src={`${assetsPath}/logo-desktop.svg`}
+        />
+      </picture>
+    );
+
+    return logoLink ?
+      React.cloneElement(logoLink, logoLink.props, content) :
+      content;
+  };
+
   return (
     <header
       role="banner"
-      className={`${styles.header} ${fixed && styles.fixed}`}
+      className={`${styles.header} ${fixed && styles.fixed} ${simple && styles.simple}`}
     >
       <div className={container}>
         <div className={styles.flex}>
           <div className={styles.pullLeft}>
-            <Logo
-              link={logoLink}
-              assetsPath={assetsPath}
-              title={title}
-            />
+            {simple ? simpleLogo() : (
+              <Logo
+                link={logoLink}
+                assetsPath={assetsPath}
+                title={title}
+              />
+            )}
+
             <span className={styles.title}>{title}</span>
           </div>
-          <div className={styles.pullRight}>
-            {children}
-          </div>
+          {!simple ? (
+            <div className={styles.pullRight}>
+              {children}
+            </div>
+          ) : null
+          }
         </div>
       </div>
     </header>
@@ -43,6 +71,7 @@ Header.propTypes = {
   fixed: PropTypes.bool,
   logoLink: PropTypes.element.isRequired,
   title: PropTypes.string,
+  simple: PropTypes.bool,
 };
 
 Header.defaultProps = {
@@ -51,4 +80,5 @@ Header.defaultProps = {
   container: 'container-fluid',
   fixed: true,
   title: '',
+  simple: false,
 };
--- packages/kununu-header/Header.spec.jsx
+++ packages/kununu-header/Header.spec.jsx
@@ -49,3 +49,50 @@ test('Renders Header without crashing', () => {
 
   expect(component).toMatchSnapshot();
 });
+
+test('Renders simple Header without crashing', () => {
+  const component = render(
+    <Header
+      title="Volle Transparenz am Arbeitsmarkt"
+      logoLink={<a href="/test">test</a>}
+      simple={true}
+    >
+      <HeaderNav>
+        <HeaderNavItem>
+          <a href="/test">
+            <span className="hidden-xs">
+              <i
+                className="fa fa-search hidden-xs"
+                aria-hidden="true"
+              />
+              &nbsp; Suchen
+            </span>
+            <i
+              className="fa fa-search visible-xs"
+              aria-hidden="true"
+            />
+          </a>
+        </HeaderNavItem>
+        <HeaderNavItem>
+          <a
+            className=""
+            href="/"
+          >
+            <span className="hidden-xs">Mein kununu </span>
+            <i className="fa fa-user visible-xs" />
+          </a>
+        </HeaderNavItem>
+        <HeaderNavItem>
+          <a
+            href="/"
+            className="btn btn-dd-sm btn-primary"
+          >
+            Firma bewerten
+          </a>
+        </HeaderNavItem>
+      </HeaderNav>
+    </Header>,
+  );
+
+  expect(component).toMatchSnapshot();
+});
