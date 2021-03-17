28035c4e6d4ea00e85f88d575bde88e567a190da
Fábio Rodrigues <fabiorsx2003@gmail.com>
2021-03-17 11:12:07 +0000

    Kuncore 1737 (#590)
    * Removed TUV from footer
    Removed all TUV related code including the Icon
    
    * Removed Boston from the list of locations
    
    * Fixed lint error and set next release version
    
    * Fixed failing test
    
    * Fixed lint issues

--- packages/client-gear-playground/icons.jsx
+++ packages/client-gear-playground/icons.jsx
@@ -71,7 +71,6 @@ import ThumbUp from '../kununu-icons/ThumbUp';
 import Trash from '../kununu-icons/Trash';
 import User from '../kununu-icons/User';
 import Verified from '../kununu-icons/Verified';
-import Tuv from '../kununu-icons/Tuv';
 
 import styles from './index.scss';
 
@@ -225,9 +224,6 @@ const Icon = () => (
         <Verified />
         <span>Verified</span>
       </div>
-    </div>
-    <div>
-      <Tuv />
     </div>
   </div>
 );
