309072768a04c817e602b941135fe2b6bdbf8151
Daniel Stein <stein-daniel@codingcircus.com>
2021-03-31 12:59:01 +0200

    Add error handling and use minimist for named args

--- packages/kununu-utils/kununu-helpers/mockBFF/create.js
+++ packages/kununu-utils/kununu-helpers/mockBFF/create.js
@@ -4,21 +4,26 @@ const path = require('path');
 
 require('isomorphic-fetch');
 
-const [,, ...args] = process.argv;
+const {
+  baseDir,
+  routes,
+  apiUrl,
+} = require('./utils');
 
-const apiUrl = 'https://www.kununu.com';
-
-const routes = require(`${process.cwd()}/${args[0]}/routes.json`); // eslint-disable-line
+// create data dir if not available
+if (!fs.existsSync(path.join(baseDir, 'data'))) {
+  fs.mkdirSync(path.join(baseDir, 'data'));
+}
 
 routes.map(async (route) => {
+  // fetch data from real api
   const data = await fetch(`${apiUrl}/${route}`);
   const result = await data.json();
 
   const mockFilePath = path.join('data', `${encodeURIComponent(route)}.json`);
+  const filePath = path.join(baseDir, mockFilePath);
 
-  const filePath = path.join(process.cwd(), args[0], mockFilePath);
-
+  // save data as json file into mock dir
   fs.writeFileSync(filePath, JSON.stringify(result));
-
-  console.log(`Created ${mockFilePath}`);
+  console.log(`Created ${mockFilePath}`); // eslint-disable-line
 });
--- packages/kununu-utils/kununu-helpers/mockBFF/server.js
+++ packages/kununu-utils/kununu-helpers/mockBFF/server.js
@@ -1,19 +1,25 @@
 #!/usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+
 const fastify = require('fastify')({logger: true});
 
-const [,, ...args] = process.argv;
+const {
+  baseDir,
+  routes,
+} = require('./utils');
 
-const baseDir = `${process.cwd()}/${args[0]}`;
+// check that data dir for mocks exists
+if (!fs.existsSync(path.join(baseDir, 'data'))) throw Error(`No mock data found at ${baseDir}. Please use the create command first.`);
 
-const routes = require(`${baseDir}/routes.json`); // eslint-disable-line
-
+// for each route serve the json file of the same name
 routes.map(route => fastify.get(`/${route}`, (_, reply) => {
   const data = require(`${baseDir}/data/${encodeURIComponent(route)}`); // eslint-disable-line
 
   reply.send(data);
 }));
 
-// Run the server!
+// Run the server
 const start = async () => {
   try {
     await fastify.listen(3001);
