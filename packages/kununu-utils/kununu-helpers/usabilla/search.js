66043c74d147182016b42e84ade5bb378055721d
Ricardo Neves <ricardofernandodasneves@gmail.com>
2021-06-17 17:53:11 +0100

    feat(usabilla): added util fn to check conditions to show usabilla search feedback.

--- packages/kununu-utils/kununu-helpers/usabilla/search.js
+++ packages/kununu-utils/kununu-helpers/usabilla/search.js
@@ -5,8 +5,15 @@ const STORAGE_USABILLA_PROFILE_SHOW = 'usabillaProfileShow';
 const STORAGE_USABILLA_PROFILE_SHOW_EXPIRE_SECONDS = 20;
 const STORAGE_USABILLA_LAST_SEARCH_EXPIRE_DAYS = 60;
 
+export const deleteProfilesVisibilityStorage = () => {
+  const cookieExpireDate = new Date();
+
+  cookieExpireDate.setDate(cookieExpireDate.getDate() - 1);
+  cookies.set(STORAGE_USABILLA_PROFILE_SHOW, false, {expires: cookieExpireDate, path: '/'});
+};
+
 export const getLastSearchStorage = () => {
-  const lastSearch = {show: false};
+  let lastSearch = {show: false};
 
   try {
     lastSearch = cookies.get(STORAGE_USABILLA_LAST_SEARCH);
@@ -15,8 +22,13 @@ export const getLastSearchStorage = () => {
   return lastSearch;
 };
 
-export const getProfilesVisibilityStorage = () => {
-  return cookies.get(STORAGE_USABILLA_PROFILE_SHOW);
+export const getProfilesVisibilityStorage = () => cookies.get(STORAGE_USABILLA_PROFILE_SHOW);
+
+export const setLastSearchStorage = (searchId, sessionId) => {
+  const cookieExpireDate = new Date();
+
+  cookieExpireDate.setDate(cookieExpireDate.getDate() + STORAGE_USABILLA_LAST_SEARCH_EXPIRE_DAYS);
+  cookies.set(STORAGE_USABILLA_LAST_SEARCH, {searchId, sessionId}, STORAGE_USABILLA_LAST_SEARCH_EXPIRE_DAYS, {expires: cookieExpireDate, path: '/'});
 };
 
 export const setProfilesVisibilityStorage = () => {
@@ -26,10 +38,4 @@ export const setProfilesVisibilityStorage = () => {
   cookies.set(STORAGE_USABILLA_PROFILE_SHOW, true, {expires: cookieExpireDate, path: '/'});
 };
 
-export const setLastSearchStorage = (searchId, sessionId) => {
-  cookies.set(STORAGE_USABILLA_LAST_SEARCH, {searchId, sessionId}, STORAGE_USABILLA_LAST_SEARCH_EXPIRE_DAYS);
-};
-
-export const shouldShow = (sessionId, searchId, lastSearch) => {
-  return sessionId && (!lastSearch.sessionId || (lastSearch.sessionId && (sessionId !== lastSearch.sessionId || searchId === lastSearch.searchId)));
-};
+export const shouldShow = (sessionId, searchId, lastSearch) => sessionId && (!lastSearch.sessionId || (lastSearch.sessionId && (sessionId !== lastSearch.sessionId || searchId === lastSearch.searchId)));
