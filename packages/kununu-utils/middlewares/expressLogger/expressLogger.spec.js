0306a6e4715521c2fac7e1de440bb62db24e9d85
Ricardo Neves <3121786+RicardoNeves@users.noreply.github.com>
2022-04-21 16:15:32 +0100

    fix: Wrong log level on expressLogger middleware. (#629)
    chore: Added mixing .npmrc files to libs.

--- packages/kununu-utils/middlewares/expressLogger/expressLogger.spec.js
+++ packages/kununu-utils/middlewares/expressLogger/expressLogger.spec.js
@@ -52,8 +52,22 @@ describe('expressLogger middleware', () => {
   it('formats log correctly on request out', async () => {
     await request(app).get('/');
 
-    expect(spyFunc.mock.calls.length).toBe(1);
+    expect(spyFunc.mock.calls.length).toBe(2);
     expect(JSON.parse(spyFunc.mock.calls[0][0])).toMatchObject({
+      message: 'Got request - GET /',
+      level: 6,
+      level_name: 'INFO',
+      datetime: new Date().toISOString(),
+      http: {
+        method: 'GET',
+        request: '/',
+        status: 200,
+        local_ip: '::ffff:127.0.0.1',
+        user_agent: 'node-superagent/3.8.3',
+      },
+      channel: 'middleware',
+    });
+    expect(JSON.parse(spyFunc.mock.calls[1][0])).toMatchObject({
       message: 'Request Out: 200 OK - GET / - origin finish',
       level: 6,
       level_name: 'INFO',
@@ -72,7 +86,7 @@ describe('expressLogger middleware', () => {
     });
   });
 
-  it('validates the json schema', async () => {
+  it('validates initial log json schema', async () => {
     const schema = applicationSchema;
 
     const ajv = new Ajv({allErrors: true});
@@ -83,6 +97,29 @@ describe('expressLogger middleware', () => {
       .set('x-forwarded-host', 'www.test.dev')
       .set('x-amzn-trace-id', 'Root=1-67891233-abcdef012345678912345678');
     const output = JSON.parse(spyFunc.mock.calls[0][0]);
+
+    const validate = await ajv.compile(schema);
+
+    const valid = validate(output);
+
+    if (!valid) {
+      defaultConsole.log(validate.errors);
+    }
+
+    expect(valid).toEqual(true);
+  });
+
+  it('validates log json schema', async () => {
+    const schema = applicationSchema;
+
+    const ajv = new Ajv({allErrors: true});
+
+    await request(app)
+      .get('/')
+      .set('x-real-ip', '127.0.0.1')
+      .set('x-forwarded-host', 'www.test.dev')
+      .set('x-amzn-trace-id', 'Root=1-67891233-abcdef012345678912345678');
+    const output = JSON.parse(spyFunc.mock.calls[1][0]);
 
     const validate = await ajv.compile(schema);
 
--- packages/kununu-utils/middlewares/expressLogger/index.js
+++ packages/kununu-utils/middlewares/expressLogger/index.js
@@ -15,10 +15,18 @@ const INFO = 'info';
 const expressLogger = application => (req, res, next) => {
   const startDate = new Date();
 
+  requestLogger.log(INFO, {
+    req,
+    res,
+    application,
+    channel: 'middleware',
+    message: `Got request - ${req.method} ${req.originalUrl}`,
+    metrics: {},
+  });
+
   function log () {
     // Remove listeners to ensure that no hanging listeners exists
     res.removeListener('finish', log);
-    res.removeListener('close', log);
     res.removeListener('error', log);
 
     // Define log level on
@@ -35,11 +43,8 @@ const expressLogger = application => (req, res, next) => {
     });
   }
 
-  // Logs successfully finished pipeline
-  res.on('finish', log.bind({origin: 'finish'}));
-
   // Logs any aborted pipeline
-  res.on('close', log.bind({level: 'error', origin: 'close'}));
+  res.on('finish', log.bind({origin: 'finish'}));
 
   // Logs any internal errors
   res.on('error', log.bind({level: 'error', origin: 'error'}));
